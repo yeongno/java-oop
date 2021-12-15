ade972506eff3d1e59ee97e16972c325dfe9a1e5
rightnow <dldudsh83@gmail.com>
2021-12-15 23:00:01 +0900

    Implement main entry point 실행될 메인 로직 작성하고 정렬 알고리즘 사용 테스트는 실행 결과만 관측하고 있음. 검증 코드는 없음.

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        //Given
+        String[] args ={"3","2","1"};
+        //When
+        Main.main(args);
+
+        //Then
+
+
+    }
+}
